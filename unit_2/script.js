66dfa484e1db019f2d07e9689cd36b51f4d31ecc
Roman <vakulka95@gmail.com>
2020-06-08 17:17:04 +0300

    second unit was done

---
+++ unit_2/script.js
@@ -0,0 +1,12 @@
+let out = document.getElementById('out');
+const myYear = 1995;
+out.innerHTML = 2020 - myYear;
+
+const myName = 'Roma'
+alert(myName);
+
+let a = 'Hello';
+let b = ' ';
+let c = 'world';
+console.log(a + b + c);
+console.log(c + b + a);
