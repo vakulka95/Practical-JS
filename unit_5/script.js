37ecc98c0b4a7ce0b67ebe1a2216c70adee270a2
Roman Vakulenko <vakulka95@gmail.com>
2020-06-12 20:44:06 +0300

    Button for add word has been created

--- unit_5/script.js
+++ unit_5/script.js
@@ -1,3 +1,4 @@
+// Task 1
 let switchOn = document.querySelector('.switch');
 
 switchOn.onclick = changeStyle;
@@ -5,4 +6,16 @@ switchOn.onclick = changeStyle;
 function changeStyle(){
     document.querySelector('link').href = 'style2.css';
     switchOn.style.display = 'none';
+}
+
+// Task 2
+
+
+let addBtn = document.querySelector('.add');
+
+addBtn.addEventListener('click', addWord);
+
+function addWord(){
+    let word = document.querySelector('.task2 p');
+    word.innerHTML += ' word';
 }
