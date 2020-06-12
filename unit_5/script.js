49c9fb08220e0aebf6948682f9f21e342ace23e5
Roman Vakulenko <vakulka95@gmail.com>
2020-06-12 21:08:07 +0300

    unit 5 was done

--- unit_5/script.js
+++ unit_5/script.js
@@ -12,10 +12,24 @@ function changeStyle(){
 
 
 let addBtn = document.querySelector('.add');
+let word = document.querySelector('.task2 p');
 
 addBtn.addEventListener('click', addWord);
 
 function addWord(){
-    let word = document.querySelector('.task2 p');
     word.innerHTML += ' word';
 }
+
+// Task 3 - 6 
+
+word.insertAdjacentHTML('afterbegin', 'any text ');
+word.insertAdjacentHTML('beforebegin', 'any second text ');
+word.insertAdjacentHTML('beforeend', ' before end text ');
+word.insertAdjacentHTML('afterend', ' after end text ');
+
+// Task 7
+
+let hi = document.querySelector('.hi');
+
+// hi.outerHTML = '<b>Hi</b>';
+hi.outerText = '<b>Hi</b>';
