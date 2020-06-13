a514c7ba477e2c22f0e876f659fa4ae3f13cd8cb
Roman Vakulenko <vakulka95@gmail.com>
2020-06-13 19:03:45 +0300

    Input range was added on the page

--- unit_6/script.js
+++ unit_6/script.js
@@ -1,3 +1,4 @@
+// Task 1 - 2;
 let text = document.querySelector('.text');
 let body = document.querySelector('body');
 
@@ -11,4 +12,13 @@ let colorBtn = document.querySelector('.color-btn').onclick = () => {
 let def = document.querySelector('.default-btn').onclick = () => {
     text.style.color = '#000000';
     body.style.backgroundColor = '#ffffff';
+}
+
+// Task 3
+
+let range = document.querySelector('.size');
+
+let sizeBtn = document.querySelector('.size-btn').onclick = () => {
+    
+    text.style.fontSize = `${range.value}px`;
 }
