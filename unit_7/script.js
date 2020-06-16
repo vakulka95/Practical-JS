2d2d8d6871edd281e7b4467e565c0898d66b0dc7
Roman Vakulenko <vakulka95@gmail.com>
2020-06-16 14:41:56 +0300

    I have created task 10

--- unit_7/script.js
+++ unit_7/script.js
@@ -78,8 +78,46 @@ for(let i = 1000; i <= 2000; i++){
 
 // Task 9
 
-let par = document.querySelectorAll('p');
+let paragraph = document.querySelectorAll('.main p');
 
-for(let i = 0; i <= par.length; i++){
-    par[i].innerHTML += i + 1;
+for(let i = 0; i < paragraph.length; i++){
+    paragraph[i].innerHTML += i + 1;
 }
+
+// Task 10
+let arrayOfZodiacs = [
+    ["Козерог", 12, 1, 22, 31, 1, 20 ],
+    ["Водолей", 1, 2, 21, 31, 1, 18 ],
+    ["Рыбы", 2, 3, 19, 28, 1, 20 ],
+    ["Овен", 3, 4, 21, 31, 1, 20 ],
+    ["Телец", 4, 5, 21, 31, 1, 21 ],
+    ["Близнецы", 5, 6, 22, 31, 1, 21 ],
+    ["Рак", 6, 7, 22, 31, 1, 22 ],
+    ["Лев", 7, 8, 23, 31, 1, 23 ],
+    ["Дева", 8, 9, 24, 31, 1, 23 ],
+    ["Весы", 9, 10, 24, 31, 1, 23 ],
+    ["Скорпион", 10, 11, 24, 31, 1, 22 ],
+    ["Стрелец", 11, 12, 23, 31, 1, 21 ],
+]
+
+
+let zodiac = document.querySelector('.zodiac-btn').onclick = () => {
+    let monthInp = document.querySelector('.month');
+    let dayInp = document.querySelector('.day');
+    let out = document.querySelector('.out_10')
+    month = parseInt(monthInp.value);
+    day = parseInt(dayInp.value);
+
+    for(let i = 0; i < arrayOfZodiacs.length; i++){
+        if(month === arrayOfZodiacs[i][1]){
+            if(day >= arrayOfZodiacs[i][3] && day <= arrayOfZodiacs[i][4]){
+                out.innerHTML = arrayOfZodiacs[i][0];
+            }
+        }else if(month === arrayOfZodiacs[i][2]){
+            if(day >= arrayOfZodiacs[i][5] && day <= arrayOfZodiacs[i][6]){
+                out.innerHTML = arrayOfZodiacs[i][0];
+            }
+        }
+    }
+};
+
