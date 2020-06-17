af7a129d7796df5bd4316dfffa7395d75745b145
Roman Vakulenko <vakulka95@gmail.com>
2020-06-17 14:18:46 +0300

    unit 7 was created

--- unit_7/script.js
+++ unit_7/script.js
@@ -160,5 +160,120 @@ function move(){
     out13.innerHTML = arr;
 }
 
+// Task 14
+let arr2 = [1, -2, 0, 5, 10, -152, 293, 2842, -1973, 0];
+let out14 = document.querySelector('.out_14');
+out14.style.margin = '20px';
+let qnt = 0;
+
+for(let i = 0; i < arr2.length; i++){
+    if(arr2[i] < 0){
+        qnt++;
+        out14.innerHTML = qnt;
+    }
+}
+
+// Task 15
+
+let out15 = document.querySelector('.out_15');
+out15.style.margin = '20px';
 
 
+// let minus = arr2.filter(elem => elem < 0)
+// console.log(minus);
+
+for(let i = 0; i < arr2.length; i++){
+    if(arr2[i] < 0){
+        out15.innerHTML += arr2[i];
+    }
+}
+
+// Task 16
+
+let createBtn = document.querySelector('.create').onclick = createArray;
+let inputCreate = document.querySelector('.array');
+let out16 = document.querySelector('.out_16');
+out16.style.margin = '20px';
+let arr16 = [];
+
+function createArray(){
+    for(let i = 0; i < inputCreate.value; i++){
+        arr16.push(1);
+    }
+    out16.innerHTML = arr16;
+}
+
+// Task 17
+
+let btn = document.querySelector('.create2').onclick = createArray;
+let create = document.querySelector('.array2');
+let out17 = document.querySelector('.out_17');
+let arr17 = [];
+
+function createArray(){
+    for(let i = 0; i < create.value; i++){
+        arr17.push(Math.floor(Math.random() * 101));
+    }
+    out17.innerHTML = arr17;
+}
+
+// Task 18
+
+let out18 = document.querySelector('.out_18');
+let btn18 = document.querySelector('.btn18');
+btn18.addEventListener('click', addElements);
+btn18.style.margin = '20px';
+let arr18_1 = [1, 5, 'Hello', 253, '13', 'Ja-Ja', 9372];
+let arr18_2 = [];
+
+function addElements(){
+    for(let i = 0; i < arr18_1.length; i++){
+        if(typeof arr18_1[i] === 'number' ){
+            arr18_2.push(arr18_1[i]);
+        }
+        out18.innerHTML = arr18_2;
+    }
+}
+
+// Task 19
+
+let out19_max = document.querySelector('.out_19_max');
+let out19_arr = document.querySelector('.out_19_arr');
+let btn19 = document.querySelector('.btn19');
+btn19.addEventListener('click', showMax);
+btn19.style.margin = '20px';
+
+let arr_19 = [1, 7, 251, 9753, -24, 0, 274, 32];
+let max = 0;
+
+function showMax(){
+    for(let i = 0; i < arr_19.length; i++){
+        if(arr_19[i] > max){
+            max = arr_19[i];
+        }
+    }
+    out19_arr.innerHTML = arr_19;
+    out19_max.innerHTML = max;
+
+}
+
+// Task 20
+
+let out20 = document.querySelector('.out_20');
+let btn20 = document.querySelector('.btn20');
+btn20.addEventListener('click', showStat);
+
+let arr_20 = [1, 5, 23, 27, 946, 5, 37, 956, 625, 12, 1, 27, 37, 37, 37, 1736,]
+let ob = {}
+function showStat(){
+    for(let i = 0; i < arr_20.length; i++){
+        if(ob[arr_20[i]] === undefined){
+            ob[arr_20[i]] = 1;
+        }else{
+            ob[arr_20[i]]++;
+        }
+    }
+    for(let el in ob){
+        out20.innerHTML += `${el}: ${ob[el]} <br>`;
+    }
+}
