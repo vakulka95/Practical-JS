d8c1456e80e976bf1391def9f0e353ebf1538fb7
Roman Vakulenko <vakulka95@gmail.com>
2020-06-18 13:21:13 +0300

    he pictures were added and task 2 was done

--- unit_8/script.js
+++ unit_8/script.js
@@ -1,7 +1,7 @@
 // Task 1
 let block = document.querySelector('.block');
-block.style.width = '500px';
-block.style.height = '500px';
+block.style.width = '300px';
+block.style.height = '300px';
 
 block.onmousedown = () => {
     return false;
@@ -16,5 +16,22 @@ block.ondblclick = () => {
 
 document.oncontextmenu = () => {
     return false;
+}
+
+// Task 2
+let block2 = document.querySelector('.block2');
+block2.style.backgroundImage = "url('img/close.png')";
+block2.style.backgroundRepeat = "no-repeat";
+block2.style.width = '400px';
+block2.style.height = '400px';
+
+block2.onmouseenter = () => {
+    block2.style.backgroundImage = "url('img/open.png')";
+block2.style.backgroundRepeat = "no-repeat";
+}
+
+block2.onmouseleave = () => {
+    block2.style.backgroundImage = "url('img/close.png')";
+block2.style.backgroundRepeat = "no-repeat";
 }
 
